44ad43eabcdb3a917a21a8fa4ad7fa36b597560e
iaconom821 <iaconom821@gmail.com>
2021-06-04 12:29:34 -0400

    validate players

--- app/models/player.rb
+++ app/models/player.rb
@@ -4,5 +4,10 @@ class Player < ApplicationRecord
     has_many :fields, through: :games
     has_secure_password
 
+    validates :name, presence: true
+    validates :age, presence: true
+    validates :username, presence: true
+    validates :password_digest, presence: true
+
     validates :skill_level, numericality: { greater_than: 0, less_than: 11, message: "Skill Level Between 1 and 10 only" }
 end
